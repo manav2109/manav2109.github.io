3197749038f05ffc2a75adbd00d6bd6158e290b2
Lakshay <lakshay940@outlook.com>
2024-08-14 18:14:28 +0530

    set padding for about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,28 +11,51 @@ function About() {
     <Container fluid className="about-section">
       <Particle />
       <Container>
-        <Row style={{ justifyContent: "center", padding: "10px" }}>
+        <Row
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh", // Full viewport height
+            textAlign: "center", // Center text horizontally
+            padding: "10px",
+          }}
+        >
           <Col
-            md={7}
+            md={12} // Full width to center the content
             style={{
-              justifyContent: "center",
-              paddingTop: "30px",
-              paddingBottom: "50px",
+              paddingTop: "0px",
+              paddingBottom: "0px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center", // Center content horizontally
             }}
           >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+            <h1 style={{ fontSize: "3em", paddingBottom: "20px", margin: "0" }}>
               Know Who <strong className="purple">I'M</strong>
             </h1>
-            <Aboutcard />
+            <div style={{ marginTop: "20px" }}>
+              <Aboutcard />
+            </div>
           </Col>
           <Col
             md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
+            style={{
+              paddingTop: "50px",
+              paddingBottom: "50px",
+              display: "flex",
+              justifyContent: "center",
+            }}
             className="about-img"
           >
-            <img src={laptopImg} alt="about" className="img-fluid-laptop-image" />
+            <img
+              src={laptopImg}
+              alt="about"
+              className="img-fluid-laptop-image"
+              style={{ maxWidth: "80%", height: "auto" }} // Adjusted max-width for better fit
+            />
           </Col>
         </Row>
+
         <h1 className="project-heading">
           Professional <strong className="purple">Skillset </strong>
         </h1>
@@ -46,7 +69,7 @@ function About() {
 
         <Github />
       </Container>
-    </Container>
+    </Container >
   );
 }
 
