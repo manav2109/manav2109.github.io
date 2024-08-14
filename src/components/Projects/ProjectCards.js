3a705d177e7d5223ec5b9be1584ac53e093b88d3
Ajay-0209 <ajay.mca21.du@gmail.com>
2024-08-14 21:50:14 +0530

    added changes

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -10,20 +10,28 @@ function ProjectCards(props) {
         <Card.Body>
           <Row>
             {/* First Section: Company Logo */}
-            <Col xs={4} className="logo-section">
+            <Col xs={5} sm={3} className="logo-section">
               <div className="logo-wrapper" style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
                 <Card.Img
                   variant="top"
                   src={props.imgPath}
                   alt="company-logo"
                   className="company-logo"
+                  style={{
+                    width:"65%",
+                    minWidth:"60%",
+                  }}
                 />
               </div>
             </Col>
-
             {/* Second Section: Company Name (Each character on a new line) */}
-            <Col xs={1} className="company-name-section" style={{ paddingLeft: "0px" }}>
-              <div className="company-name-wrapper" style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100%" }}>
+            <Col 
+              xs={1} 
+              sm={1} 
+              className="company-name-section" 
+              style={{ paddingLeft: "0px", paddingRight: "0px", marginLeft: "-10px" }} // Reduced the margin between sections
+            >
+              <div className="company-name-wrapper" style={{ display: "flex", flexDirection: "column", alignItems: "centre",alignContent:"space-between",flexWrap:"wrap", justifyContent: "center", height: "100%" }}>
                 {props.name.split("").map((char, index) => (
                   <div
                     key={index}
@@ -40,7 +48,7 @@ function ProjectCards(props) {
             </Col>
 
             {/* Third Section: Upper Text and Description */}
-            <Col xs={7} className="text-description-section">
+            <Col xs={6} sm={8} className="text-description-section">
               <div className="upper-text" style={{ paddingBottom: "5%" }}>
                 {/* You can add any upper text content here */}
                 <span>{props.title}</span>
