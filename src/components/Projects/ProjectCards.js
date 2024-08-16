73c0806806a2055a27a7dfbbbd653ac30ec0cf70
Ajay-0209 <ajay.mca21.du@gmail.com>
2024-08-16 21:19:51 +0530

    added changes for small screen sizes

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,44 +1,82 @@
+import React, { useEffect, useState } from "react";
 import { Card, Row, Col } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
+  const [isXs, setIsXs] = useState(window.innerWidth <= 600);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsXs(window.innerWidth <= 600);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="project-card-container">
-      <Card className="project-card-view" style={{ width: '100%' }}>
+      <Card className="project-card-view" style={{ width: "100%" }}>
         <Card.Body>
           <Row>
             {/* First Section: Company Logo */}
-            <Col xs={5} sm={3} className="logo-section">
-              <div className="logo-wrapper" style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
+            <Col
+              xs={{ span: 12, order: 1 }}
+              sm={3}
+              className="logo-section"
+            >
+              <div
+                className="logo-wrapper"
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  height: "100%",
+                }}
+              >
                 <Card.Img
                   variant="top"
                   src={props.imgPath}
                   alt="company-logo"
                   className="company-logo"
                   style={{
-                    width:"65%",
-                    minWidth:"60%",
+                    width: "65%",
+                    minWidth: "60%",
                   }}
                 />
               </div>
             </Col>
+
             {/* Second Section: Company Name (Each character on a new line) */}
-            <Col 
-              xs={1} 
-              sm={1} 
-              className="company-name-section" 
-              style={{ paddingLeft: "0px", paddingRight: "0px", marginLeft: "-10px" }} // Reduced the margin between sections
+            <Col
+              xs={{ span: 12, order: 2 }}
+              sm={1}
+              className="company-name-section"
+              style={{
+                paddingLeft: "0px",
+                paddingRight: "0px",
+                marginLeft: "-10px",
+              }}
             >
-              <div className="company-name-wrapper" style={{ display: "flex", flexDirection: "column", alignItems: "centre",alignContent:"space-between",flexWrap:"wrap", justifyContent: "center", height: "100%" }}>
+              <div
+                className="company-name-wrapper"
+                style={{
+                  display: "flex",
+                  flexDirection: isXs ? "row" : "column", // Adjust based on screen size
+                  alignContent: "space-between",
+                  flexWrap: "wrap",
+                  justifyContent: "center",
+                  height: "100%",
+                }}
+              >
                 {props.name.split("").map((char, index) => (
                   <div
                     key={index}
                     style={{
-                      fontSize: "2.5rem", // Adjust the size as needed
+                      fontSize: "2.5rem",
                       fontWeight: "bold",
-                      lineHeight: "3.0rem", // Adjust line height to ensure proper spacing
+                      lineHeight: "3.0rem",
                     }}
                   >
                     {char}
@@ -48,9 +86,15 @@ function ProjectCards(props) {
             </Col>
 
             {/* Third Section: Upper Text and Description */}
-            <Col xs={6} sm={8} className="text-description-section">
-              <div className="upper-text" style={{ paddingBottom: "5%" }}>
-                {/* You can add any upper text content here */}
+            <Col
+              xs={{ span: 12, order: 3 }}
+              sm={8}
+              className="text-description-section"
+            >
+              <div
+                className="upper-text"
+                style={{ paddingBottom: "5%" }}
+              >
                 <span>{props.title}</span>
               </div>
               <div className="description">
