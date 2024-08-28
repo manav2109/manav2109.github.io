32a618c6cfad7c0b5fc9d20bbaae88eeb4dd02dd
Ajay-0209 <ajay.mca21.du@gmail.com>
2024-08-28 14:52:56 +0530

    added some margin in About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
 import Toolstack from "./Toolstack";
-
+import "./About.css";
 function About() {
   return (
     <Container fluid className="about-section">
@@ -56,7 +56,7 @@ function About() {
           </Col>
         </Row>
 
-        <h1 className="project-heading">
+        <h1 className="project-heading" >
           Professional <strong className="purple">Skillset </strong>
         </h1>
 
